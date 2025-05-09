9149533eac8d3c4c0bf1a9a4a1edcccf32ba59da
willianmv <willianvmarques300@gmail.com>
2025-05-09 19:04:52 -0300

    refactor(security): implementa filtros de requisição para /roles

--- src/main/java/com/example/blog/Controllers/RoleController.java
+++ src/main/java/com/example/blog/Controllers/RoleController.java
@@ -8,6 +8,7 @@ import com.example.blog.services.RoleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -31,6 +32,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/v1/tags").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/tags/**").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/api/v1/roles").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/roles").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/roles/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
                 )
